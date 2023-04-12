9ad46541cb148df56e96301bcdfd3a256c342fe1
Josip Bošnjak <jbosnjak3@gmail.com>
2023-04-12 20:47:07 +0200

    added application to path

---
+++ DynamicPictureGallery/src/main/java/dynamic/picture/gallery/DynamicPictureGalleryApplication.java
@@ -0,0 +1,13 @@
+package dynamic.picture.gallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicPictureGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicPictureGalleryApplication.class, args);
+	}
+
+}
---
+++ DynamicPictureGallery/src/test/java/dynamic/picture/gallery/DynamicPictureGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package dynamic.picture.gallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicPictureGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
