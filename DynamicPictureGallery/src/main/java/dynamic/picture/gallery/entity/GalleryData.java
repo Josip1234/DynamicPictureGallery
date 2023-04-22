35d7b12efef7b7136f0e51ffa6583143e5988ae0
Josip Bošnjak <jbosnjak3@gmail.com>
2023-04-22 10:53:11 +0200

    Updated user class, added Gallery data

---
+++ DynamicPictureGallery/src/main/java/dynamic/picture/gallery/entity/GalleryData.java
@@ -0,0 +1,43 @@
+package dynamic.picture.gallery.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class GalleryData {
+	@NonNull
+    private String galleryName;
+	//trebamo neku listu slika dodati ćemo to kasnije
+	private Integer currentIndex;
+	private Integer previousIndex;
+	private Integer nextIndex;
+	private Integer firstIndex;
+	private Integer lastIndex;
+	
+	
+	public Integer increaseIndex() {
+		return this.currentIndex++;
+	}
+	public Integer decreaseIndex() {
+		return this.currentIndex--;
+	}
+	public Integer returnFirstIndex() {
+		return this.firstIndex=getFirstIndex();
+	}
+	public Integer returnLastIndex() {
+		return this.lastIndex=getLastIndex();
+	}
+    public void rememberPreviousIndex(Integer currentIndex) {
+    	this.previousIndex=currentIndex;
+    }
+    public void setCurrentIndex(Integer index){
+    	this.currentIndex=index;
+    }
+    public void setNextIndex(Integer index) {
+    	this.nextIndex=index;
+    }
+}
--- DynamicPictureGallery/src/main/java/dynamic/picture/gallery/entity/User.java
+++ DynamicPictureGallery/src/main/java/dynamic/picture/gallery/entity/User.java
@@ -7,4 +7,5 @@ import lombok.Data;
 @AllArgsConstructor
 public class User {
 private String nickname;
+private String password;
 }
