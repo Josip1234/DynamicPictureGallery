79281f2f5a71359205a623520d9d680dfc978435
Josip Bošnjak <jbosnjak3@gmail.com>
2023-05-01 14:05:49 +0200

    added index page

---
+++ DynamicPictureGallery/src/main/java/dynamic/picture/gallery/controllers/HomePageController.java
@@ -0,0 +1,15 @@
+package dynamic.picture.gallery.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/DynamicPictureGallery/")
+public class HomePageController {
+
+	@GetMapping("/")
+	public String getHomePage() {
+		return "index";
+	}
+}
