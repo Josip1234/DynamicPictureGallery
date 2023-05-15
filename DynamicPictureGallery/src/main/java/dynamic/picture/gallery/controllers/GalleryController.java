564cdebdca561ebb68a3bea2cfa8c722ff5428f4
Josip Bošnjak <jbosnjak3@gmail.com>
2023-05-15 20:07:52 +0200

    gallery data succesfully inserted into database

--- DynamicPictureGallery/src/main/java/dynamic/picture/gallery/controllers/GalleryController.java
+++ DynamicPictureGallery/src/main/java/dynamic/picture/gallery/controllers/GalleryController.java
@@ -1,12 +1,13 @@
 package dynamic.picture.gallery.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import ch.qos.logback.core.model.Model;
+
 import dynamic.picture.gallery.entity.GalleryData;
 import dynamic.picture.gallery.entity.User;
 import dynamic.picture.gallery.functions.GeneralFunctions;
@@ -32,7 +33,8 @@ public class GalleryController {
 	@PostMapping("/galleryName")
 	public String insertIntoGallery(@ModelAttribute("GalleryData") GalleryData data) {
 		String email=GeneralFunctions.getUserEmail();
-		System.out.println(email);
+		String us= dataRepository.findByEmailAddress(email);
+		data.setUsername(us);
 		dataRepository.save(data);
 		return "redirect:/uploadFile";
 	}
--- DynamicPictureGallery/src/main/java/dynamic/picture/gallery/repository/GalleryDataRepository.java
+++ DynamicPictureGallery/src/main/java/dynamic/picture/gallery/repository/GalleryDataRepository.java
@@ -1,9 +1,15 @@
 package dynamic.picture.gallery.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import dynamic.picture.gallery.entity.GalleryData;
+import dynamic.picture.gallery.entity.User;
 
 public interface GalleryDataRepository extends JpaRepository<GalleryData, Integer> {
+	  @Query(value = "SELECT username FROM USER WHERE EMAIL = ?", nativeQuery = true)
+	  String findByEmailAddress(String emailAddress);
 
 }
