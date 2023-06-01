e73558e42e31b623a9002b92a70b5b9357f3080a
Josip Bošnjak <jbosnjak3@gmail.com>
2023-06-01 19:23:41 +0200

    update

--- DynamicPictureGallery/src/main/java/dynamic/picture/gallery/DynamicPictureGalleryApplication.java
+++ DynamicPictureGallery/src/main/java/dynamic/picture/gallery/DynamicPictureGalleryApplication.java
@@ -1,7 +1,13 @@
 package dynamic.picture.gallery;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import dynamic.picture.gallery.repository.ImageStorageService;
+
+
 
 @SpringBootApplication
 public class DynamicPictureGalleryApplication {
@@ -9,5 +15,11 @@ public class DynamicPictureGalleryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DynamicPictureGalleryApplication.class, args);
 	}
-
+	@Bean
+	CommandLineRunner init(ImageStorageService storageService) {
+		return (args) -> {
+			
+			storageService.init();
+		};
+	}
 }
